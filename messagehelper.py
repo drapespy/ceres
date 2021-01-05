592892f700a4209065676dfd3a14e0e7d425c75a
lildrapes <66695324+lildrapes@users.noreply.github.com>
2021-01-04 20:06:22 -0800

    Update messagehelper.py

--- messagehelper.py
+++ messagehelper.py
@@ -1,10 +1,15 @@
 import discord
 from discord.ext import commands
+import datetime
 
 client = commands.Bot(command_prefix = 'm.')
 
 @client.event
 async def on_ready():
     print('Bot is now Online!')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'The Bots ping is {round(client.latency * 1000)}ms')
 
 client.run('Nzg0MTY2MDE0NDc2NjE1Njkx.X8lVgg.f62CYt-37qsOxiihDM828TXuawM')
