9e941fd41c982f7b67ae33f28f67a2ca28f2f1bc
lildrapes <66695324+lildrapes@users.noreply.github.com>
2021-01-06 17:19:14 -0800

    Update messagehelper.py

--- messagehelper.py
+++ messagehelper.py
@@ -6,12 +6,9 @@ client = commands.Bot(command_prefix = 'm.')
 
 @client.event
 async def on_ready():
-    print('Bot is now updated.')
-
-@client.event
-async def on_ready():
-    activity = discord.Game(name="m.help", type=2)
-    await client.change_presence(status=discord.Status.online, activity=activity)
+    activity = discord.(name="m.commands", type=3)
+await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="m.commands"))
+    print("Bot is ready!")
 
 @client.command()
 async def commands(ctx):
