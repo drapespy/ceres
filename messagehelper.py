f0733435a26c5d2c8dbe3a8e1050ad2d6534f86a
lildrapes <66695324+lildrapes@users.noreply.github.com>
2021-01-07 17:43:51 -0800

    Update messagehelper.py

--- messagehelper.py
+++ messagehelper.py
@@ -10,17 +10,26 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name="m.help | dsc.gg/xydev"))
     print("Bot is ready!")
 
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(':x: That is not a valid command')
 
 @client.command()
 async def help(ctx):
     await ctx.send('**__Stats__**\n`m.help` - Shows This Help Message\n`m.ping` - Shows the bots latency\n**__Messages__**\n`m.purge <amount>` - Clears the given amount of messages!')
 
 @client.command(aliases=['clear'])
-async def purge(ctx, amount=2):
+async def purge(ctx, amount : int):
     await ctx.channel.purge(limit=amount)
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f"`Pinging...`\nMessageHelper's current ping is **{round(client.latency * 1000)}**ms")
+
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(':x: Please specify a number of messages to delete.')
 
 client.run('Nzg0MTY2MDE0NDc2NjE1Njkx.X8lVgg.f62CYt-37qsOxiihDM828TXuawM')
