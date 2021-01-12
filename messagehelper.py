7283c1ca98c89d6dc966a740fa166eaf6336f133
lildrapes <66695324+lildrapes@users.noreply.github.com>
2021-01-11 18:08:02 -0800

    Update messagehelper.py

--- messagehelper.py
+++ messagehelper.py
@@ -25,6 +25,10 @@ async def help(ctx):
     embed.set_footer(text="My prefix is m.")
     await ctx.send(embed=embed)
 
+@client.command
+async def bhunkymunky(ctx):
+    await ctx.send(f"Subscribe to Bhunky Munky [here](https://www.youtube.com/channel/UCIN_VAFZhU6977tGmiVYlZg?sub_confirmation=1)")
+
 @client.command(aliases=['clear'])
 @commands.has_permissions(manage_messages=True)
 async def purge(ctx, amount : int):
