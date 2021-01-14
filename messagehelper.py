2f7fe9458c7652d5f95b72b633cdc0bb30b415cd
lildrapes <66695324+lildrapes@users.noreply.github.com>
2021-01-14 14:26:50 -0800

    Update messagehelper.py

--- messagehelper.py
+++ messagehelper.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import datetime
 import asyncio
 
-client = commands.Bot(command_prefix = 'm.' , 'M.')
+client = commands.Bot(command_prefix = 'm.')
 client.remove_command("help")
 
 @client.event
